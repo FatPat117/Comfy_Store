42ada9d06371d51f08c4a7f3ff9ccf1e0dd5a6c0
Phat117 <ntphat110705@gmail.com>
2025-07-04 22:05:24 +0700

    About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function About() {
-        return <h1>About </h1>;
+        return (
+                <section className="align-element grid place-items-center">
+                        <div className=" flex items-center">
+                                <h1 className="text-center text-6xl text-base-content shrink-0">We love</h1>
+                                <div className="stat">
+                                        <div className="stat-title text-primary-content text-6xl font-bold p-6 bg-primary rounded-xl">
+                                                ComFy
+                                        </div>
+                                </div>
+                        </div>
+
+                        <p className="mt-6 text-center text-base-content/80 max-w-2xl text-xl ">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est minus eos excepturi. Sit
+                                reiciendis quidem doloremque officiis eligendi id quia repellat doloribus quod,
+                                repudiandae temporibus enim illo rem et vero laborum. Repellat minus quaerat at sequi,
+                                nam distinctio voluptatem sunt placeat labore quae sit quibusdam, praesentium sint
+                                provident. Suscipit, dolores.
+                        </p>
+                </section>
+        );
 }
