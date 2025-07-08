11edc478a87446e29bda09452b7b0e71942651f3
Phat117 <ntphat110705@gmail.com>
2025-07-08 23:16:47 +0700

    Products Container

--- src/components/ProductsContainer.jsx
+++ src/components/ProductsContainer.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsFillGridFill, BsList } from "react-icons/bs";
 import { useLoaderData } from "react-router-dom";
-import { ProductsList } from ".";
+import { ProductsGrid, ProductsList } from ".";
 
 export default function ProductsContainer() {
-        const { products } = useLoaderData();
+        const { products, meta } = useLoaderData();
+        const totalProducts = meta.pagination.total;
+
+        const [layout, setLayout] = useState("grid");
+
+        const setActiveLayout = (pattern) => {
+                return `text-xl btn btn-circle btn-sm ${
+                        layout === pattern
+                                ? "btn-active btn-primary text-primary-content"
+                                : "btn-ghost text-base-content"
+                }`;
+        };
+
         return (
                 <>
-                        <ProductsList />
+                        {/* Header */}
+                        <div className="flex justify-between items-center mt-8 border-b border-base-300 pb-5">
+                                <h4 className="font-medium text-lg capitalize">
+                                        {totalProducts} product{totalProducts > 1 ? "s" : ""} found
+                                </h4>
+                                <div className="flex items-center gap-x-2">
+                                        <button className={setActiveLayout("grid")} onClick={() => setLayout("grid")}>
+                                                <BsFillGridFill />
+                                        </button>
+                                        <button className={setActiveLayout("list")} onClick={() => setLayout("list")}>
+                                                <BsList />
+                                        </button>
+                                </div>
+                        </div>
+                        {/* Products */}
+                        <div>
+                                {totalProducts === 0 ? (
+                                        <h5 className="text-2xl mt-16">Sorry, no products matched your search</h5>
+                                ) : layout === "grid" ? (
+                                        <ProductsGrid />
+                                ) : (
+                                        <ProductsList />
+                                )}
+                        </div>
                 </>
         );
 }
