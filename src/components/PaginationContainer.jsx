b00294c6ac578ee22ba54026470af1e52b119a1e
Phat117 <ntphat110705@gmail.com>
2025-07-10 16:29:26 +0700

    Pagination #1

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,48 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 export default function PaginationContainer() {
-        return <div></div>;
+        const { meta } = useLoaderData();
+        const { pageCount, page: currentPage } = meta.pagination;
+        const pages = Array.from({ length: pageCount }, (_, index) => index + 1);
+
+        if (pageCount < 2) return null;
+
+        const handlePageChange = (page) => {
+                console.log(page);
+        };
+
+        return (
+                <div>
+                        <div className="mt-12 flex items-center justify-end">
+                                <div className="join">
+                                        <button
+                                                className="btn btn-xs md:btn-md join-item"
+                                                onClick={() => handlePageChange(currentPage - 1)}
+                                        >
+                                                Prev
+                                        </button>
+                                        {pages.map((pageNumber) => (
+                                                <button
+                                                        key={pageNumber}
+                                                        className={`join-item btn btn-xs sm:btn-md ${
+                                                                pageNumber === currentPage
+                                                                        ? "btn-active bg-base-300 border-gray-300 rounded-md"
+                                                                        : ""
+                                                        }`}
+                                                        onClick={() => handlePageChange(pageNumber)}
+                                                >
+                                                        {pageNumber}
+                                                </button>
+                                        ))}
+                                        <button
+                                                className="btn btn-xs md:btn-md join-item"
+                                                onClick={() => handlePageChange(currentPage + 1)}
+                                        >
+                                                Next
+                                        </button>
+                                </div>
+                        </div>
+                </div>
+        );
 }
